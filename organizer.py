ad1b60b4ed02d51360b4cb46e2979662c0d0070d
xdarksyderx <xdarksyderx@pop-os.localdomain>
2020-10-17 20:06:25 +0200

    Multiple files name functionality added

--- organizer.py
+++ organizer.py
@@ -1,6 +1,19 @@
 import os
 import shutil
 
+
+def move_files(original_name, new_name, to_move, counter):
+    if not(new_name in os.listdir(to_move)):
+        os.rename(original_name, new_name)
+        print('Moviendo', new_name, 'a', to_move)
+        shutil.move(new_name, to_move)
+    
+    else:
+        n_name = '({})'.format(counter) + original_name
+        print(n_name)
+        move_files(original_name, n_name, to_move, counter+1)        
+
+
 def organize(path, folders):
     EXTENSIONS = {'AUDIOVISUAL':['jpg', 'png', 'mp4', 'mp3', 'jpeg', 'raw'], 'TEXT':['doc', 'odt', 'pdf', 'ppt', 'txt'], 'COMPRESSED_FOLDER': ['zip', 'rar', '7z'], 'CODE': ['py', 'c', 'cpp', 'html', 'css', 'pyw', 'cs', 'js', 'php']}
     os.chdir(path)
@@ -14,24 +27,28 @@ def organize(path, folders):
                 file_extension = path_container[i].split('.')[1]
 
                 if file_extension in EXTENSIONS['AUDIOVISUAL']:
-                    shutil.move(path_container[i], './AUDIOVISUAL')
+                    move_files(path_container[i], path_container[i], './AUDIOVISUAL', 1)
+
 
                 elif file_extension in EXTENSIONS['TEXT']:
-                    shutil.move(path_container[i], './TEXT')
+                    move_files(path_container[i], path_container[i], './TEXT', 1)
 
+                
                 elif file_extension in EXTENSIONS['COMPRESSED_FOLDER']:
-                    shutil.move(path_container[i], './COMPRESSED_FOLDER')
+                    move_files(path_container[i], path_container[i], './COMPRESSED_FOLDER', 1)
 
+                
                 elif file_extension in EXTENSIONS['CODE']:
-                    shutil.move(path_container[i], './CODE')
+                    move_files(path_container[i], path_container[i], './CODE', 1)
 
+                
                 else:
-                    shutil.move(path_container[i], './OTHER')
+                    move_files(path_container[i], path_container[i], './OTHER', 1)
 
                 
             except:
-                shutil.move(path_container[i], './COMPRESSED_FOLDER')    
-
+                #shutil.move(path_container[i], './COMPRESSED_FOLDER')    
+                pass
 
 
         
